32bd260a58fca2ee035aaf493f9cd09e20315844
Martin Jacob Rehder <gitrepocon01@rehsco.com>
2021-05-10 09:16:16 +0200

    Initial Commit

---
+++ StringSizeCalculationExtensions/StringSizeCalculationExtensions.h
@@ -0,0 +1,18 @@
+//
+//  StringSizeCalculationExtensions.h
+//  StringSizeCalculationExtensions
+//
+//  Created by Martin Rehder on 10.05.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StringSizeCalculationExtensions.
+FOUNDATION_EXPORT double StringSizeCalculationExtensionsVersionNumber;
+
+//! Project version string for StringSizeCalculationExtensions.
+FOUNDATION_EXPORT const unsigned char StringSizeCalculationExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StringSizeCalculationExtensions/PublicHeader.h>
+
+
